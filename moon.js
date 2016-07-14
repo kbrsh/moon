9836b5ed1590d2a092956c9fd9fb88c1ef0a4b75
kingsofcoding <simplecooldude1@gmail.com>
2016-07-14 16:16:59 +0000

    replace all with regex

--- moon.js
+++ moon.js
@@ -4,14 +4,14 @@
         var el = document.querySelector(opts.el),
             model = opts.model,
             children = el.childNodes,
-            start = "{{",
-            end = "}}";
+            start = opts.start || "{{",
+            end = opts.end || "}}";
             
         // one way data binding
         for (var key in model) {
             if (model.hasOwnProperty(key)) {
                 var pattern = start + key + end;
-                el.innerHTML = el.innerHTML.replace(pattern, model[key]);
+                el.innerHTML = el.innerHTML.replace(new RegExp(pattern, 'g'), model[key]);
             }
         }   
     }
