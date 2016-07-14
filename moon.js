19f00db12fa95fdaad6960f1817107e05bcf1cb4
kingsofcoding <simplecooldude1@gmail.com>
2016-07-14 17:08:15 +0000

    persisting?

--- moon.js
+++ moon.js
@@ -3,18 +3,35 @@
     function Moon(opts) {
         var el = document.querySelector(opts.el),
             model = opts.model,
-            children = el.childNodes,
             start = opts.start || "{{",
             end = opts.end || "}}";
+            
             
         // one way data binding
         for (var key in model) {
             if (model.hasOwnProperty(key)) {
-                var pattern = start + key + end;
-                el.innerHTML = el.innerHTML.replace(new RegExp(pattern, 'g'), model[key]);
+                var obj = {};
+                Object.defineProperty(obj, key, {
+                    get: function() {
+                        return model[key];
+                    },
+                    set: function(val) {
+                        model[key] = val;
+                        update(start, end, key, model, el);
+                    }
+                });
+                update(start, end, key, model, el);
             }
         }
     }
+    
+    var update = function(start, end, key, model, el) {
+        var pattern = start + key + end;
+        el.innerHTML = el.innerHTML.replace(new RegExp(pattern, 'g'), model[key]);
+    }
+    
+    window.Moon = Moon;
+    
     
     
 })(window);
