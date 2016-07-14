d7b34d0a733fd8a13c9f2c76c585def2dca9020d
kingsofcoding <simplecooldude1@gmail.com>
2016-07-14 16:15:08 +0000

    start end variables

--- moon.js
+++ moon.js
@@ -3,12 +3,15 @@
     function Moon(opts) {
         var el = document.querySelector(opts.el),
             model = opts.model,
-            children = el.childNodes
+            children = el.childNodes,
+            start = "{{",
+            end = "}}";
             
         // one way data binding
         for (var key in model) {
             if (model.hasOwnProperty(key)) {
-                el.innerHTML = el.innerHTML.replace("{{" + key + "}}", model[key]);
+                var pattern = start + key + end;
+                el.innerHTML = el.innerHTML.replace(pattern, model[key]);
             }
         }   
     }
