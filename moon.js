1500a9b65feb883ee773c34a3a29615044e53eae
kingsofcoding <simplecooldude1@gmail.com>
2016-07-14 05:05:52 +0000

    get all children for el

--- moon.js
+++ moon.js
@@ -2,7 +2,8 @@
     
     function Moon(opts) {
         var el = document.querySelector(opts.el),
-            model = opts.model;
+            model = opts.model,
+            children = el[0].children;
             
         
     }
