94f0a45f1af820cea4ac12b2ea44c22d02c9c397
Kabir Shah <simplecooldude1@gmail.com>
2016-10-23 15:06:28 -0700

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var gulp = require('gulp');
+var pkg = require('./package.json');
+var comment = '\/*\r\n* Moon ' + pkg.version + '\r\n* Copyright 2016, Kabir Shah\r\n* https:\/\/github.com\/KingPixil\/moon\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kingpixil.github.io\/license\r\n*\/\r\n';
+var $ = require('gulp-load-plugins')();
+
+gulp.task('build', function () {
+  return gulp.src(['./src/core.js'])
+    .pipe($.concat('moon.js'))
+    .pipe($.header(comment + '\n'))
+    .pipe($.size())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify', ['build'], function() {
+  return gulp.src(['./dist/moon.min.js'])
+    .pipe(uglify())
+    .pipe($.header(comment))
+    .pipe($.size())
+    .pipe($.concat('moon.min.js'))
+    .pipe(gulp.dest('./dist/'));
+});
+
+
+
+gulp.task('default', ['build', 'minify']);
