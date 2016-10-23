867d29ec726695a41eb67ea5ee6b23defe2c26b1
Kabir Shah <simplecooldude1@gmail.com>
2016-10-23 15:02:21 -0700

    tidy

--- src/core.js
+++ src/core.js
@@ -1,40 +1,43 @@
-function Moon(opts) {
-  var _el = opts.el;
-  var _data = opts.data;
-  var _logs = opts.logs;
-  this.el = document.getElementById(_el);
-  this.html = this.el.innerHTML;
-  this.logs = _logs;
+(function(window) {
+    function Moon(opts) {
+        var _el = opts.el;
+        var _data = opts.data;
+        var _logs = opts.logs;
+        this.el = document.getElementById(_el);
+        this.html = this.el.innerHTML;
+        this.logs = _logs;
 
-  if(this.logs) {
-    console.log('%c ======= 🌚 MOON 🌚 ======= ', 'font-size: 2rem; color: #010edc');
-    console.log("%c Initial Data =>", "font-size: 1rem; color: #018d55");
-    console.log("%c \t Element: " + JSON.stringify(_el), "font-size: 0.8rem");
-    console.log("%c \t Data: " + JSON.stringify(_data), "font-size: 0.8rem");
-  }
+        if (this.logs) {
+            console.log('%c ======= 🌚 MOON 🌚 ======= ', 'font-size: 2rem; color: #010edc');
+            console.log("%c Initial Data =>", "font-size: 1rem; color: #018d55");
+            console.log("%c \t Element: " + JSON.stringify(_el), "font-size: 0.8rem");
+            console.log("%c \t Data: " + JSON.stringify(_data), "font-size: 0.8rem");
+        }
 
-  Object.defineProperty(this, 'data', {
-    get: function() {
-      return _data;
-    },
-    set: function(value) {
-      _data = value;
-      this.build();
+        Object.defineProperty(this, 'data', {
+            get: function() {
+                return _data;
+            },
+            set: function(value) {
+                _data = value;
+                this.build();
+            }
+        });
+
+
+        this.build = function() {
+            var generated = this.html;
+            for (prop in this.data) {
+                generated = generated.replace(new RegExp("{{" + prop + "}}", "gi"), this.data[prop]);
+            }
+            this.el.innerHTML = generated;
+        }
+
+
+
+        this.build();
     }
-  });
 
+    window.Moon = Moon;
 
-  this.build = function() {
-    var generated = this.html;
-    for(prop in this.data) {
-      generated = generated.replace(new RegExp("{{" + prop + "}}", "gi"), this.data[prop]);
-    }
-    this.el.innerHTML = generated;
-  }
-
-
-
-  this.build();
-}
-
-window.Moon = Moon;
+})(window);
