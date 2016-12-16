69fd0b8d7dd7c1ff93b8c0236169a9cdc8a796d2
Gopal Shah <simplecooldude1@gmail.com>
2016-12-15 21:54:43 -0800

    don't set m-component

--- src/core.js
+++ src/core.js
@@ -106,8 +106,6 @@
                 componentDummy.setAttribute(attr, componentProps[attr]);
               }
 
-              componentDummy.setAttribute("m-component", "");
-
               componentFound.outerHTML = componentDummy.outerHTML;
             }
           }
