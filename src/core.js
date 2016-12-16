a8e2cbea9f59972d1e9b6c89a77c94a1729b22a7
Gopal Shah <simplecooldude1@gmail.com>
2016-12-15 18:10:02 -0800

    get component

--- src/core.js
+++ src/core.js
@@ -80,7 +80,7 @@
             }
 
             if(this.components[el.type.toLowerCase()]) {
-              
+              var component = this.components[el.type.toLowerCase()];
             }
 
             this.build(el.children);
