2ec74b6037c09c80054beadb80bac7830db85983
Kabir Shah <simplecooldude1@gmail.com>
2016-12-17 15:20:51 -0800

    merge with second one

--- src/core.js
+++ src/core.js
@@ -56,7 +56,7 @@
     */
     function merge(obj, obj2) {
       for (var key in obj2) {
-        if (obj2.hasOwnProperty(key)) obj[key] = obj[key];
+        if (obj2.hasOwnProperty(key)) obj[key] = obj2[key];
       }
       return obj;
     }
