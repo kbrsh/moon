05c947a9aca8748d63fe4bb22d7eff06ec0c1872
Kabir Shah <simplecooldude1@gmail.com>
2016-12-17 10:24:20 -0800

    put them back to get access to local props

--- src/core.js
+++ src/core.js
@@ -187,6 +187,43 @@
           _methods[method]();
         }
 
+        // Default Directives
+        directives["m-if"] = function(el, val, vdom) {
+          var evaluated = new Function("return " + val);
+          if(!evaluated()) {
+            el.textContent = "";
+          } else {
+            el.textContent = compileTemplate(vdom.val, self.$data);
+          }
+        }
+
+        directives["m-on"] = function(el, val, vdom) {
+          var splitVal = val.split(":");
+          var eventToCall = splitVal[0];
+          var methodToCall = splitVal[1];
+          el.addEventListener(eventToCall, function() {
+            self.method(methodToCall);
+          });
+          el.removeAttribute("m-on");
+          delete vdom.props["m-on"];
+        }
+
+        directives["m-model"] = function(el, val, vdom) {
+          el.value = self.get(val);
+          el.addEventListener("input", function() {
+            self.set(val, el.value);
+          });
+          el.removeAttribute("m-model");
+          delete vdom.props["m-model"];
+        }
+
+        directives["m-once"] = function(el, val, vdom) {
+          vdom.val = el.textContent;
+          for(var child in vdom.children) {
+            vdom.children[child].val = compileTemplate(vdom.children[child].val, self.$data);
+          }
+        }
+
         // directives["m-for"] = function(el, val, vdom) {
         //   var splitVal = val.split(" in ");
         //   var alias = splitVal[0];
@@ -273,43 +310,6 @@
     */
     Moon.directive = function(name, action) {
       directives["m-" + name] = action;
-    }
-
-    // Default Directives
-    directives["m-if"] = function(el, val, vdom) {
-      var evaluated = new Function("return " + val);
-      if(!evaluated()) {
-        el.textContent = "";
-      } else {
-        el.textContent = compileTemplate(vdom.val, self.$data);
-      }
-    }
-
-    directives["m-on"] = function(el, val, vdom) {
-      var splitVal = val.split(":");
-      var eventToCall = splitVal[0];
-      var methodToCall = splitVal[1];
-      el.addEventListener(eventToCall, function() {
-        self.method(methodToCall);
-      });
-      el.removeAttribute("m-on");
-      delete vdom.props["m-on"];
-    }
-
-    directives["m-model"] = function(el, val, vdom) {
-      el.value = self.get(val);
-      el.addEventListener("input", function() {
-        self.set(val, el.value);
-      });
-      el.removeAttribute("m-model");
-      delete vdom.props["m-model"];
-    }
-
-    directives["m-once"] = function(el, val, vdom) {
-      vdom.val = el.textContent;
-      for(var child in vdom.children) {
-        vdom.children[child].val = compileTemplate(vdom.children[child].val, self.$data);
-      }
     }
 
     window.Moon = Moon;
