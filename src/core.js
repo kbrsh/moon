711e1ff5fbcf88ed9341c65c2589ce582ab10570
Gopal Shah <simplecooldude1@gmail.com>
2016-12-16 18:18:06 -0800

    m pre

--- src/core.js
+++ src/core.js
@@ -202,6 +202,10 @@
           delete vdom.props["m-model"];
         }
 
+        directives["m-pre"] = function(el, val, vdom) {
+          
+        }
+
         // directives["m-for"] = function(el, val, vdom) {
         //   var splitVal = val.split(" in ");
         //   var alias = splitVal[0];
