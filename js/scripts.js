62d3767b2d5b4b108df7b2080c752a6a302b30ae
Gopal Shah <simplecooldude1@gmail.com>
2016-12-23 10:30:30 -0800

    change to dollar sign

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,17 @@ var app2 = new Moon({
     msg: 'Hello Moon!'
   }
 });
+
+var app3 = new Moon({
+  el: '#app3',
+  data: {
+    msg: 'Hello Moon!'
+  },
+  methods: {
+    change: function() {
+      app3.$set('msg', 'Changed the Value!');
+    }
+  }
+});
+
+app3.$method('change');
