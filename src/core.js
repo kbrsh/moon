0f99d722f4017fa0883c5a0f297a2009ab7b0351
Kabir Shah <simplecooldude1@gmail.com>
2016-12-25 12:16:28 -0800

    delete componenets to html

--- src/core.js
+++ src/core.js
@@ -292,7 +292,6 @@
           if(_hooks.created) {
             _hooks.created();
           }
-          this.componentsToHTML();
           this.createVirtualDOM(this.$el);
           this.build(this.$dom.children);
           if(_hooks.mounted) {
