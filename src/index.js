bf655aab2dd926ab03a05093e0883d09746e0d2b
Gopal Shah <simplecooldude1@gmail.com>
2016-12-28 12:10:29 -0800

    require global api

--- src/index.js
+++ src/index.js
@@ -42,57 +42,7 @@
 
     //=require instance/methods.js
 
-    /* ======= Global API ======= */
-
-    /**
-    * Sets the Configuration of Moon
-    * @param {Object} opts
-    */
-    Moon.config = function(opts) {
-      if(opts.silent) {
-        config.silent = opts.silent;
-      }
-    }
-
-    /**
-    * Runs an external Plugin
-    * @param {Object} plugin
-    */
-    Moon.use = function(plugin) {
-      plugin.init(Moon);
-    }
-
-    /**
-    * Creates a Directive
-    * @param {String} name
-    * @param {Function} action
-    */
-    Moon.directive = function(name, action) {
-      directives["m-" + name] = action;
-    }
-
-    /**
-    * Creates a Component
-    * @param {String} name
-    * @param {Function} action
-    */
-    Moon.component = function(name, opts) {
-      components[name] = opts;
-    }
-
-    /**
-    * Creates Subclass of Moon
-    * @param {Object} opts
-    */
-    Moon.extend = function(opts) {
-      var Parent = this;
-      function MoonComponent() {
-        Moon.call(this, opts);
-      }
-      MoonComponent.prototype = Object.create(Parent.prototype);
-      MoonComponent.prototype.constructor = MoonComponent;
-      return MoonComponent;
-    }
+    //=require global/api.js
 
     window.Moon = Moon;
 })(window);
