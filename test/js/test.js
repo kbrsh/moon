6820c9d30ae1b1e604a0f6f39ea44999c1c0e38b
Gopal Shah <simplecooldude1@gmail.com>
2016-12-27 22:56:48 -0800

    no jquery

--- test/js/test.js
+++ test/js/test.js
@@ -4,3 +4,15 @@ describe('Initializing', function() {
     expect(new Moon({el: "#initialize"}) instanceof Moon).to.equal(true);
   });
 });
+
+describe('Data', function() {
+  it('while initializing', function() {
+    var dataApp = new Moon({
+      el: "#data",
+      data: {
+        msg: "Hello Moon!"
+      }
+    });
+    expect(document.getElementById("data").innerHTML).to.equal("Hello Moon!");
+  });
+});
