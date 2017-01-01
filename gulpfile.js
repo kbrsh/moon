7d7b44de2679ef664bd4b95646b660599294371a
Gopal Shah <simplecooldude1@gmail.com>
2017-01-01 14:58:40 -0800

    comment

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require("gulp-uglifyjs")
 var comment = '\/*\r\n* Moon ' + pkg.version + '\r\n* Copyright 2016, Kabir Shah\r\n* https:\/\/github.com\/KingPixil\/moon\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kingpixil.github.io\/license\r\n*\/\r\n';
 var $ = require('gulp-load-plugins')();
 
+// Build Moon
 gulp.task('build', function () {
   return gulp.src(['./src/index.js'])
     .pipe($.include())
@@ -15,6 +16,7 @@ gulp.task('build', function () {
     .pipe(gulp.dest('./dist/'));
 });
 
+// Build minified (compressed) version of Moon
 gulp.task('minify', ['build'], function() {
   return gulp.src(['./dist/moon.js'])
     .pipe(uglify())
