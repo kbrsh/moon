6ec006ca5fec6c8e85ad116ed646771fd0a9598c
Kabir Shah <simplecooldude1@gmail.com>
2017-01-11 15:55:43 -0800

    use childnodes

--- dist/moon.js
+++ dist/moon.js
@@ -143,10 +143,9 @@
     function Moon(opts) {
         /* ======= Initial Values ======= */
         opts = opts || {};
-        var _el = opts.el;
-        var _data = opts.data;
         var self = this;
-        this.$el = document.querySelector(_el);
+        var _data = opts.data;
+        this.$el = document.querySelector(opts.el);
         this.$template = opts.template || this.$el.innerHTML;
         this.$hooks = merge({created: noop, mounted: noop, updated: noop, destroyed: noop}, opts.hooks);
         this.$methods = opts.methods || {};
