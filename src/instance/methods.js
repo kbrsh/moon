7e776205f8303cba3905678ff1c82bda7f4f669e
Gopal Shah <simplecooldude1@gmail.com>
2017-01-11 21:36:11 -0800

    don't put that, won't get called anyway

--- src/instance/methods.js
+++ src/instance/methods.js
@@ -24,7 +24,6 @@ Moon.prototype.error = function(msg) {
 * @param {String} val
 */
 Moon.prototype.set = function(key, val) {
-  if(!this.$data[key]) this.error("Don't set new data properties here, instead, declare them when creating the instance");
   this.$data[key] = val;
   if(!this.$destroyed) this.build(this.$el.childNodes, this.$dom.children);
   this.$hooks.updated();
