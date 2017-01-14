721476396ecb000a6d76562fe97b0223602b5976
Kabir Shah <simplecooldude1@gmail.com>
2017-01-13 21:26:33 -0800

    different log message for tests

--- test/test.js
+++ test/test.js
@@ -1,1 +1,1 @@
-console.log("    Open:\n \t\033[0;34m'file://" + __dirname + "/test.html'\033[0;0m\n \tin your browser to see Moon tests.");
+console.log("Open:\n  \033[0;34m'file://" + __dirname + "/test.html'\033[0;0m\n  in your browser to see Moon tests.");
