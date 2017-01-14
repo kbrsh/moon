2b0a5ac4535aba8742da7686ac62f05b7c55aab0
Kabir Shah <simplecooldude1@gmail.com>
2017-01-13 17:19:26 -0800

    escape the newlines in template

--- src/util/util.js
+++ src/util/util.js
@@ -12,6 +12,7 @@ var compileTemplate = function(template, data) {
   code.replace(templateRe, function(match, key) {
     code = code.replace(match, "' + data['" + key + "'] + '");
   });
+  code = code.replace("\n", "' + \n'");
   var compile = new Function("data", "var out = '" + code + "'; return out");
   var output = compile(data);
   return output;
