4f1b8fd4067c7ad86b4fdb66af9a270857d9736d
Kabir Shah <simplecooldude1@gmail.com>
2017-01-15 18:21:13 -0800

    set it to compiled version by default

--- src/util/util.js
+++ src/util/util.js
@@ -51,19 +51,13 @@ var createElement = function(type, val, props, children, meta, node) {
 */
 var createVirtualDOM = function(node) {
   var tag = node.nodeName;
-  var content = "";
+  var content = compileTemplate(node.textContent);
   var attrs = extractAttrs(node);
   var defaultMeta = {
     once: false,
     shouldRender: true
   }
   var children = [];
-
-  if(tag === "#text") {
-    content = compileTemplate(node.textContent);
-  } else {
-    content = node.textContent;
-  }
 
   for(var i = 0; i < node.childNodes.length; i++) {
     children.push(createVirtualDOM(node.childNodes[i]));
