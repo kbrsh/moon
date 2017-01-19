2b115bc21d580acf9445d85a75adf7607abd9a77
Kabir Shah <simplecooldude1@gmail.com>
2017-01-18 21:24:10 -0800

    optimize for future builds

--- dist/moon.js
+++ dist/moon.js
@@ -421,6 +421,7 @@
           if(vnode.type === "#text") {
             var valueOfVNode = "";
             valueOfVNode = vnode.val(this.$data);
+            if(vnode.node.textContent === valueOfVNode) vnode.meta.shouldRender = false;
             vnode.node.textContent = valueOfVNode;
           } else if(vnode.props) {
             for(var attr in vnode.props) {
