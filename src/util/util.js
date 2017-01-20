bf587f25f35cdad6fe83397cca05bab3848af8bb
Kabir Shah <simplecooldude1@gmail.com>
2017-01-19 17:20:08 -0800

    use child

--- src/util/util.js
+++ src/util/util.js
@@ -84,7 +84,7 @@ var createVirtualDOM = function(node) {
 var renderVirtualDOM = function(vdom, data) {
   for(var i = 0; i < vdom.children.length; i++) {
     var child = vdom.children[i];
-    child.compiled = compileTemplate(children.val)(data);
+    child.compiled = compileTemplate(child.val)(data);
     if(child.children) {
       child.children = renderVirtualDOM(child.children);
     }
