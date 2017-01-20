30837d0748617c14b86308af5633ad56c5d460c1
Kabir Shah <simplecooldude1@gmail.com>
2017-01-19 17:50:14 -0800

    render, no optimize

--- src/util/util.js
+++ src/util/util.js
@@ -86,9 +86,6 @@ var renderVirtualDOM = function(vdom, data) {
   for(var i = 0; i < vdom.children.length; i++) {
     var child = vdom.children[i];
     child.compiled = compileTemplate(child.val)(data);
-    if(child.compiled === child.val) {
-      child.meta.shouldRender = false;
-    }
     if(child.props) {
       for(var prop in child.props) {
         child.compiledProps[prop] = compileTemplate(child.props[prop])(data);
