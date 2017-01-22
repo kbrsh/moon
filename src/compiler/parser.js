50678f2f85d573d31d5617b0bebf3e1b33789911
Kabir Shah <simplecooldude1@gmail.com>
2017-01-21 21:51:47 -0800

    not working parser

--- src/compiler/parser.js
+++ src/compiler/parser.js
@@ -1,3 +1,25 @@
-var parse = function(tokens) {
-  
+var parse = function(input) {
+  var current = 0;
+  var ast = {children: []};
+  var WHITESPACE_RE = /\s/;
+  while(current < input.length) {
+    var char = input[current];
+    var next = input[current+1] || null;
+    if(char === "<") {
+    	if(next === "/") {
+      	current++;
+        continue;
+      }
+    	current++;
+    	var tag = "";
+    	while(input[current] !== ">") {
+      	tag += input[current];
+        current++;
+      }
+      ast.children.push({type: tag});
+      continue;
+    }
+    current++;
+  }
+  return ast;
 }
