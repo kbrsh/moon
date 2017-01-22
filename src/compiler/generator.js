25288792efc430eee4d33a5e3590f62093eeb152
Kabir Shah <simplecooldude1@gmail.com>
2017-01-21 22:19:51 -0800

    generate el

--- src/compiler/generator.js
+++ src/compiler/generator.js
@@ -1,15 +1,24 @@
-// var generate = function(ast) {
-//
-//   var code = JSON.stringify(ast);
-//   code.replace(TEMPLATE_RE, function(match, key) {
-//     code = code.replace(match, "' + data['" + key + "'] + '");
-//   });
-//   var render = new Function("data", "var out = '" + code + "'; return out");
-//   return render;
-// }
-//
-// var generate = function(ast) {
-//   var TEMPLATE_RE = /{{([A-Za-z0-9_.()\[\]]+)}}/gi;
-//   var code = "return " + generateEl(ast);
-//   return new Function("data", "createElement", code)
-// }
+var generateEl = function(el) {
+	var code = "";
+	for(var i = 0; i < el.children.length; i++) {
+  	var child = el.children[i];
+  	code += `createElement("${child.type}")`;
+  }
+  return code;
+}
+
+var generate = function(ast) {
+
+  var code = JSON.stringify(ast);
+  code.replace(TEMPLATE_RE, function(match, key) {
+    code = code.replace(match, "' + data['" + key + "'] + '");
+  });
+  var render = new Function("data", "var out = '" + code + "'; return out");
+  return render;
+}
+
+var generate = function(ast) {
+  var TEMPLATE_RE = /{{([A-Za-z0-9_.()\[\]]+)}}/gi;
+  var code = "return " + generateEl(ast);
+  return new Function("data", "createElement", code)
+}
