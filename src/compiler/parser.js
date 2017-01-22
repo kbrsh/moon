5b6ed1c0330a1e865676704abde8741e0d7be402
Kabir Shah <simplecooldude1@gmail.com>
2017-01-22 13:01:41 -0800

    basic lexer without attribute support

--- src/compiler/parser.js
+++ src/compiler/parser.js
@@ -1,25 +1,3 @@
-var parse = function(input) {
-  var current = 0;
-  var ast = {children: []};
-  var WHITESPACE_RE = /\s/;
-  while(current < input.length) {
-    var char = input[current];
-    var next = input[current+1] || null;
-    if(char === "<") {
-    	if(next === "/") {
-      	current++;
-        continue;
-      }
-    	current++;
-    	var tag = "";
-    	while(input[current] !== ">") {
-      	tag += input[current];
-        current++;
-      }
-      ast.children.push({type: tag});
-      continue;
-    }
-    current++;
-  }
-  return ast;
+var parse = function(tokens) {
+  
 }
