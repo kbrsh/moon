1511e329a935e232ccf85384b75b24292f8281ea
Kabir Shah <simplecooldude1@gmail.com>
2017-01-21 22:19:33 -0800

    comment gen

--- src/compiler/generator.js
+++ src/compiler/generator.js
@@ -1,9 +1,15 @@
-var generate = function(ast) {
-  var TEMPLATE_RE = /{{([A-Za-z0-9_.()\[\]]+)}}/gi;
-  var code = JSON.stringify(ast);
-  code.replace(TEMPLATE_RE, function(match, key) {
-    code = code.replace(match, "' + data['" + key + "'] + '");
-  });
-  var render = new Function("data", "var out = '" + code + "'; return out");
-  return render;
-}
+// var generate = function(ast) {
+//
+//   var code = JSON.stringify(ast);
+//   code.replace(TEMPLATE_RE, function(match, key) {
+//     code = code.replace(match, "' + data['" + key + "'] + '");
+//   });
+//   var render = new Function("data", "var out = '" + code + "'; return out");
+//   return render;
+// }
+//
+// var generate = function(ast) {
+//   var TEMPLATE_RE = /{{([A-Za-z0-9_.()\[\]]+)}}/gi;
+//   var code = "return " + generateEl(ast);
+//   return new Function("data", "createElement", code)
+// }
