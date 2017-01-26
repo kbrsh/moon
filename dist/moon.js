d035afa8db6758fc00e0b1a6e4730e3c32d6c7ca
Kabir Shah <simplecooldude1@gmail.com>
2017-01-25 17:28:40 -0800

    make merge anonymous

--- dist/moon.js
+++ dist/moon.js
@@ -81,12 +81,12 @@
      * @param {Object} obj2
      * @return {Object} Merged Objects
      */
-    function merge(obj, obj2) {
+    var merge = function (obj, obj2) {
       for (var key in obj2) {
         if (obj2.hasOwnProperty(key)) obj[key] = obj2[key];
       }
       return obj;
-    }
+    };
     
     /**
      * Does No Operation
