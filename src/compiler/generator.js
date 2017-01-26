c3048f9f3d86ce121dabdb3da9ab48ee680d634b
Kabir Shah <simplecooldude1@gmail.com>
2017-01-25 16:31:18 -0800

    fix up code generator

--- src/compiler/generator.js
+++ src/compiler/generator.js
@@ -1,25 +1,27 @@
 var generateEl = function(el) {
 	var code = "";
-	for(var i = 0; i < el.children.length; i++) {
-  	var child = el.children[i];
-    if(child.children) {
-      child.children = child.children.map(generateEl);
-    }
-    if(typeof child === "string") {
-      code += "h(null, null, \"" + child + "\")";
-    } else {
-      code += "h(\"" + child.type + "\", " + JSON.stringify(child.props) + ", " + child.children.join(",") + ")";
-    }
-  }
+	if(typeof el === "string") {
+		code += `"${el}"`;
+	} else {
+		// Recursively generate code for children
+		el.children = el.children.map(generateEl);
+		code += `h("${el.type}", ${JSON.stringify(el.props)}, ${el.children.join(",")})`;
+	}
   return code;
 }
 
 var generate = function(ast) {
+	// Matches a template string
   var TEMPLATE_RE = /{{([A-Za-z0-9_.()\[\]]+)}}/gi;
-  var code = "return " + generateEl(ast);
+	// Get root element
+	var root = ast.children[0];
+  var code = "return " + generateEl(root);
+
+	// Compile Templates
   code.replace(TEMPLATE_RE, function(match, key) {
     code = code.replace(match, '" + this.get("' + key + '") + "');
   });
+
   try {
     return new Function("h", code);
   } catch(e) {
