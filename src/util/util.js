785ee7c9c9e956291fb291815cfa43a051e2585b
Kabir Shah <simplecooldude1@gmail.com>
2017-01-28 17:52:26 -0800

    provide data for custom directives

--- src/util/util.js
+++ src/util/util.js
@@ -152,7 +152,17 @@ var diff = function(node, vnode, parent) {
   } else if(nodeName === "#text" && typeof vnode === "string") {
     node.textContent = vnode;
   } else if(vnode.type) {
-    diffProps(node, extractAttrs(node), vnode.props);
+    var nodeProps = extractAttrs(node);
+
+    diffProps(node, nodeProps, vnode.props);
+
+    nodeProps = extractAttrs(node);
+    for(var propName in nodeProps) {
+      if(directives[propName]) {
+        directives[propName](node, nodeProps[propName], vnode);
+      }
+    }
+
     for(var i = 0; i < vnode.children.length || i < node.childNodes.length; i++) {
       diff(node.childNodes[i], vnode.children[i], node);
     }
