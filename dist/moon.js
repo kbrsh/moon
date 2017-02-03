b338930969d2dddb585c5cfe17f86e8d8fd873bb
Kabir Shah <simplecooldude1@gmail.com>
2017-02-02 21:55:01 -0800

    add support for custom parameters in m-on

--- dist/moon.js
+++ dist/moon.js
@@ -810,6 +810,15 @@
      */
     Moon.prototype.callMethod = function (method, args) {
       args = args || [];
+      var full = method.split("(");
+      var customParams;
+      if (full[1]) {
+        method = full[0];
+        customParams = full[1].slice(0, -1);
+        var paramsToArr = new Function("return [" + customParams + "]");
+        customParams = paramsToArr();
+        args = args.concat(customParams);
+      }
       this.$methods[method].apply(this, args);
     };
     
