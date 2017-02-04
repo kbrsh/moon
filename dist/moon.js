b5360c301e410da1e4ddf044ded1a96c6a9156b5
Kabir Shah <simplecooldude1@gmail.com>
2017-02-03 18:08:55 -0800

    v0.2.0

--- dist/moon.js
+++ dist/moon.js
@@ -1,5 +1,5 @@
 /*
-* Moon 0.1.3
+* Moon 0.2.0
 * Copyright 2016-2017, Kabir Shah
 * https://github.com/KingPixil/moon/
 * Free to use under the MIT license.
@@ -1037,7 +1037,7 @@
     /**
      * Version of Moon
      */
-    Moon.version = '0.1.3';
+    Moon.version = '0.2.0';
     
     /**
      * Moon Utilities
