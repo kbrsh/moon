47540902948eba863a433e58c14c733e942b7ddf
Kabir Shah <simplecooldude1@gmail.com>
2017-02-14 18:22:23 -0800

    add tests for removing events

--- test/js/test.js
+++ test/js/test.js
@@ -494,12 +494,13 @@ describe('Functional Component', function() {
 
 describe("Events", function() {
   var bus = new Moon();
-  var evt1 = false, evt1_2 = false, globalEvt = false;
+  var evt1 = false, evt1_2 = false, handler1, globalEvt = false;
   describe("Handler", function() {
     it("should create an event listener", function() {
-      bus.on('evt1', function() {
+      handler1 = function() {
         evt1 = true;
-      });
+      }
+      bus.on('evt1', handler1);
       expect(bus.$events.evt1[0]).to.be.a("function");
     });
     it("should create multiple event listeners", function() {
@@ -524,6 +525,13 @@ describe("Events", function() {
     });
     it("should call the global handler", function() {
       expect(globalEvt).to.be.true;
+    });
+  });
+
+  describe("Removing", function() {
+    it("should remove a handler", function() {
+      bus.off('evt1', handler1);
+      expect(bus.$events.evt1.length).to.equal(1);
     });
   });
 });
