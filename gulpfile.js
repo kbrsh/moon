886cceeb194efe06b2b76fb7a9d76444908e9716
Kabir Shah <simplecooldude1@gmail.com>
2017-02-15 21:49:57 -0800

    add saucelabs

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,9 @@ var concat = require("gulp-concat");
 var header = require("gulp-header");
 var size = require("gulp-size");
 
+var saucelabs = require('gulp-saucelabs');
+var connect = require('gulp-connect');
+
 var comment = '\/*\r\n* Moon v' + pkg.version + '\r\n* Copyright 2016-2017, Kabir Shah\r\n* https:\/\/github.com\/KingPixil\/moon\/\r\n* Free to use under the MIT license.\r\n* https:\/\/kingpixil.github.io\/license\r\n*\/\r\n';
 
 // Build Moon
@@ -73,6 +76,82 @@ gulp.task('test', ['instrument'], function () {
         console.log("[Moon] Generated Coverage Report");
       });
 });
+
+// Saucelabs
+gulp.task('saucelabs', function() {
+  return saucelabs({
+    username: "KingPixil",
+    key: "a3594d36-e04e-4265-8bd6-3c86bac76ddc",
+    urls: ['http://localhost:3000/test/test.html'],
+    testname: 'Moon',
+    framework: 'mocha',
+    browsers: [
+      {
+        browserName: 'chrome',
+        version: '55',
+        platform: 'windows 7'
+      },
+      {
+        browserName: 'chrome',
+        version: '46',
+        platform: 'windows 7'
+      },
+      {
+        browserName: 'firefox',
+        version: '45',
+        platform: 'windows 7'
+      },
+      {
+        browserName: 'safari',
+        version: 'latest',
+        platform: 'OS X 10.10'
+      },
+      {
+        browserName: 'internet explorer',
+        version: '8',
+        platform: 'windows 8'
+      },
+      {
+        browserName: 'internet explorer',
+        version: '9',
+        platform: 'windows 8'
+      },
+      {
+        browserName: 'internet explorer',
+        version: '10',
+        platform: 'windows 10'
+      },
+      {
+        browserName: 'internet explorer',
+        version: '11',
+        platform: 'windows 10'
+      },
+      {
+        browserName: 'iphone',
+        version: '8.4',
+        platform: 'ios 10'
+      },
+      {
+        browserName: 'android',
+        version: '5.1',
+        platform: 'latest'
+      }
+    ],
+    onException: function(err) {
+      console.log(err)
+    }
+  });
+});
+
+gulp.task('saucelabs:connect', function() {
+    connect.server({ port: 3000, root: './' });
+});
+
+gulp.task('saucelabs:disconnect', () => {
+    connect.serverClose();
+});
+
+gulp.task('test-saucelabs', ['saucelabs:connect', 'saucelabs'], () => gulp.start('saucelabs:disconnect'));
 
 // Default task
 gulp.task('default', ['build', 'minify']);
