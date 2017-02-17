3ab5675e4e5df65c54cbbded9f18250a2a1572c5
Kabir Shah <simplecooldude1@gmail.com>
2017-02-16 20:35:35 -0800

    check for self closing tag or unclosed tag, fixes #8

--- dist/moon.js
+++ dist/moon.js
@@ -629,13 +629,128 @@
       };
     
       while (state.current < tokens.length) {
-        var child = walk(state);
+        var child = walk(state, root);
         if (child) {
           root.children.push(child);
         }
       }
     
       return root;
+    };
+    
+    var HTML_ELEMENTS = {
+      "html": true,
+      "body": true,
+      "base": false,
+      "head": true,
+      "link": false,
+      "meta": false,
+      "style": true,
+      "title": true,
+      "address": true,
+      "article": true,
+      "aside": true,
+      "footer": true,
+      "header": true,
+      "h1": true,
+      "h2": true,
+      "h3": true,
+      "h4": true,
+      "h5": true,
+      "h6": true,
+      "hgroup": true,
+      "nav": true,
+      "section": true,
+      "div": true,
+      "dd": true,
+      "dl": true,
+      "dt": true,
+      "figcaption": true,
+      "figure": true,
+      "hr": false,
+      "img": false,
+      "li": true,
+      "main": true,
+      "ol": true,
+      "p": true,
+      "pre": true,
+      "ul": true,
+      "a": true,
+      "b": true,
+      "abbr": true,
+      "bdi": true,
+      "bdo": true,
+      "br": false,
+      "cite": true,
+      "code": true,
+      "data": true,
+      "dfn": true,
+      "em": true,
+      "i": true,
+      "kbd": true,
+      "mark": true,
+      "q": true,
+      "rp": true,
+      "rt": true,
+      "rtc": true,
+      "ruby": true,
+      "s": true,
+      "samp": true,
+      "small": true,
+      "span": true,
+      "strong": true,
+      "sub": true,
+      "sup": true,
+      "time": true,
+      "u": true,
+      "var": true,
+      "wbr": false,
+      "area": false,
+      "audio": true,
+      "map": true,
+      "track": false,
+      "video": true,
+      "embed": false,
+      "object": true,
+      "param": false,
+      "source": false,
+      "canvas": true,
+      "script": true,
+      "noscript": true,
+      "del": true,
+      "ins": true,
+      "caption": true,
+      "col": false,
+      "colgroup": true,
+      "table": true,
+      "thead": true,
+      "tbody": true,
+      "td": true,
+      "th": true,
+      "tr": true,
+      "button": true,
+      "datalist": true,
+      "fieldset": true,
+      "form": true,
+      "input": false,
+      "label": true,
+      "legend": true,
+      "meter": true,
+      "optgroup": true,
+      "option": true,
+      "output": true,
+      "progress": true,
+      "select": true,
+      "textarea": true,
+      "details": true,
+      "dialog": true,
+      "menu": true,
+      "menuitem": true,
+      "summary": true,
+      "content": true,
+      "element": true,
+      "shadow": true,
+      "template": true
     };
     
     var createParseNode = function (type, props, children) {
@@ -646,7 +761,7 @@
       };
     };
     
-    var walk = function (state) {
+    var walk = function (state, parentNode) {
       var token = state.tokens[state.current];
       var previousToken = state.tokens[state.current - 1];
       var secondToken = state.tokens[state.current + 1];
@@ -680,16 +795,20 @@
         var startContentIndex = state.current;
         // Make sure it has content and is closed
         if (token) {
+          if (!HTML_ELEMENTS[node.type]) {
+            return node;
+          }
           // Find Closing Tag, and push children recursively
           while (token.type !== "tagStart" || token.type === "tagStart" && !token.close) {
             // Push a parsed child to the current node
-            var parsedChildState = walk(state);
+            var parsedChildState = walk(state, parentNode);
             if (parsedChildState) {
               node.children.push(parsedChildState);
             }
             increment(0);
             if (!token) {
               // No token means that there is nothing left to parse in this element
+              // This usually means that the tag was unclosed
               break;
             }
           }
