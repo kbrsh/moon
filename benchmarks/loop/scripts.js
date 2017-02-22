ca637737ad03de0c33186929cb9ff41a6a84ee0d
Kabir Shah <simplecooldude1@gmail.com>
2017-02-21 21:11:22 -0800

    add loop benchmarks

---
+++ benchmarks/loop/scripts.js
@@ -0,0 +1,70 @@
+var items = [];
+
+var add = function(num) {
+  for(var i = 0; i < num; i++) {
+    items.push("item - " + i);
+  }
+}
+
+console.time('init');
+var app = new Moon({
+  el: "#app",
+  data: {
+    items: items
+  },
+  methods: {
+    add: function() {
+      add(1000);
+      console.time('add 1000');
+      app.set('items', items);
+      Moon.nextTick(function() {
+        console.timeEnd('add 1000');
+      });
+    },
+    addLot: function() {
+      add(10000);
+      console.time('add 10000');
+      app.set('items', items);
+      Moon.nextTick(function() {
+        console.timeEnd('add 10000');
+      });
+    },
+    swap: function() {
+      var i1 = Math.floor(Math.random()*items.length);
+      var i2 = Math.floor(Math.random()*items.length);
+      var tmp = items[i1];
+      items[i1] = items[i2];
+      items[i2] = tmp;
+      console.time('swap');
+      app.set('items', items);
+      Moon.nextTick(function() {
+        console.timeEnd('swap');
+      });
+    },
+    remove: function() {
+      items.splice(Math.floor(Math.random()*items.length), 1);
+      console.time('remove');
+      app.set('items', items);
+      Moon.nextTick(function() {
+        console.timeEnd('remove');
+      });
+    },
+    clear: function() {
+      items = [];
+      console.time('clear');
+      app.set('items', items);
+      Moon.nextTick(function() {
+        console.timeEnd('clear');
+      });
+    },
+    reverse: function() {
+      items = items.reverse();
+      console.time('reverse');
+      app.set('items', items);
+      Moon.nextTick(function() {
+        console.timeEnd('reverse');
+      });
+    }
+  }
+})
+console.timeEnd('init')
