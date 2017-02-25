759ea23d15f68d202b4f4d936794fc06b0abcb85
Kabir Shah <simplecooldude1@gmail.com>
2017-02-25 09:28:48 -0800

    support creating components when replacing a child

--- dist/moon.js
+++ dist/moon.js
@@ -398,6 +398,10 @@
         // Different types, replace it
         var newNode = createNodeFromVNode(vnode, instance);
         parent.replaceChild(newNode, node);
+        if (vnode.meta.component) {
+          // Detected parent component, build it here (parent node is available)
+          createComponentFromVNode(newNode, vnode, vnode.meta.component);
+        }
         return newNode;
       } else if (vnode.meta.shouldRender && vnode.type === "#text" && nodeName === "#text" && vnode.val !== node.textContent) {
         // Both are textnodes, update the node
