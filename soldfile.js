3f1678107d130007b1bfa8243147a3a9d325e1f8
Kabir Shah <simplecooldude1@gmail.com>
2017-02-25 17:58:26 -0800

    use latest sold version, add config to add links to sidebar easily and have active class

--- soldfile.js
+++ soldfile.js
@@ -1,5 +1,15 @@
 var Sold = require("sold")
 Sold(__dirname)
+  .configHandlebars(function(Handlebars) {
+    Handlebars.registerHelper('link', function(title, postFile, actualFile) {
+      if (postFile == actualFile) {
+        return new Handlebars.SafeString(`<a href="./${actualFile}" class="sidebar-link-active">${title}</a>`);
+      }
+      else {
+        return new Handlebars.SafeString(`<a href="./${actualFile}">${title}</a>`);
+      }
+    });
+  })
   .template("template")
   .source("src")
   .postSource("docs")
