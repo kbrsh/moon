d80f592a2e9374f8dd6018d5d36f4fff7c3638a7
Kabir Shah <simplecooldude1@gmail.com>
2017-03-02 17:53:50 -0800

    add prefix option api docs

--- js/post.js
+++ js/post.js
@@ -11,7 +11,7 @@ var compile = function(val, lang) {
 
   var STR_RE = /["'](.*?)["']/g;
   var SPECIAL_RE = /\b(new|var|if|do|function|while|switch|for|foreach|in|continue|break|return)(?=[^\w])/g;
-  var GLOBAL_VARIABLE_RE = /\b(document|window|Array|String|true|false|Object|this|Number|\$)(?=[^\w])/g;
+  var GLOBAL_VARIABLE_RE = /\b(document|window|Array|String|true|false|Object|this|Boolean|Number|\$)/g;
   var METHODS_RE = /\b(indexOf|match|replace|toString|length)(?=[^\w])/g;
   var MULTILINE_COMMENT_RE  = /(\/\*.*\*\/)/g;
   var COMMENT_RE = /(\/\/.*)/g;
@@ -65,5 +65,8 @@ for(var i = 0; i < code.length; i++) {
       el.classList.remove(val);
     }
     el.innerHTML = compile(el.innerHTML, lang);
+  }
+  if(!attrs.length) {
+    el.innerHTML = compile(el.innerHTML, 'js');
   }
 }
--- src/docs/api.md
+++ src/docs/api.md
@@ -10,7 +10,7 @@ Moon comes with a global `Moon.config` object that can be used to set any custom
 
 ##### **silent**
 
-- Type: `boolean`
+- Type: `Boolean`
 - Default: `false`
 
 Usage:
@@ -19,3 +19,15 @@ Moon.config.silent = true;
 ```
 
 Can toggle all logs (excluding errors)
+
+##### **prefix**
+
+- Type: `String`
+- Default: `"m"`
+
+Usage:
+```js
+Moon.config.prefix = "data-m";
+```
+
+Can set the prefix used for directives (ie `data-m-if` instead of `m-if`). Just remember, all directives are not present in runtime.
