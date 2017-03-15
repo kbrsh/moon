be0e57d461b330f7782698b510eb8d97921285e7
Kabir Shah <simplecooldude1@gmail.com>
2017-03-14 18:09:24 -0700

    begin cache for computed properties

--- src/observer/observer.js
+++ src/observer/observer.js
@@ -1,5 +1,23 @@
 function Observer(instance) {
   this.instance = instance;
+  this.cache = {};
+}
+
+Observer.prototype.getComputed = function(prop) {
+  // Check if Changed ("dirty")
+  if(this.cache[prop].dirty) {
+    // Invoke Getter
+    var output = this.cache[prop].getter.call(this.instance);
+
+    // Cache Output
+    this.cache[prop].cache = output;
+
+    // Return Output
+    return output;
+  } else {
+    // Clean, return Cached Value
+    return this.cache[prop].cache;
+  }
 }
 
 Observer.prototype.notify = function() {
