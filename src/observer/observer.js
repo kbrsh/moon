4e7fdaeb42309e1e489d4bbec0cc57e8435985b6
Kabir Shah <simplecooldude1@gmail.com>
2017-03-23 16:43:58 -0700

    remove cache

--- src/observer/observer.js
+++ src/observer/observer.js
@@ -3,23 +3,6 @@ function Observer(instance) {
   this.cache = {};
 }
 
-Observer.prototype.getComputed = function(prop) {
-  // Check if Changed ("dirty")
-  if(this.cache[prop].dirty) {
-    // Invoke Getter
-    var output = this.cache[prop].getter.call(this.instance);
-
-    // Cache Output
-    this.cache[prop].cache = output;
-
-    // Return Output
-    return output;
-  } else {
-    // Clean, return Cached Value
-    return this.cache[prop].cache;
-  }
-}
-
 Observer.prototype.notify = function() {
   queueBuild(this.instance);
 }
