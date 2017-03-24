bb591c1da6df015bbc25accc03ad506d67414c93
Kabir Shah <simplecooldude1@gmail.com>
2017-03-23 17:59:34 -0700

    reset dependency map every time recalculating dependencies

--- src/observer/computed.js
+++ src/observer/computed.js
@@ -29,6 +29,7 @@ var initComputed = function(instance, computed) {
         if(changed) {
           // Dependencies changed, recalculate dependencies, cache the output, and return it
           instance.$observer.dep.target = prop;
+          instance.$observer.dep.map[prop] = [];
           cache = computed[prop].get.call(instance);
           instance.$observer.cache[prop] = cache;
           instance.$observer.dep.target = null;
