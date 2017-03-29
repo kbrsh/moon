176cbeebecea13089c01e142c9932ba94c0826c3
Kabir Shah <simplecooldude1@gmail.com>
2017-03-28 20:56:32 -0700

    :zap: perf boost: handle case when there are no children and remove all of them

--- dist/moon.js
+++ dist/moon.js
@@ -767,10 +767,20 @@
             newText.meta.el = node.firstChild;
           }
         } else {
-          // Just Iterate through All Children
-          var totalLen = newLength > oldLength ? newLength : oldLength;
-          for (var i = 0; i < totalLen; i++) {
-            diff(oldVNode.children[i], vnode.children[i], node, instance);
+          if (newLength === 0) {
+            // No Children, Remove all Children if not Already Removed
+            if (oldLength !== 0) {
+              var firstChild = null;
+              while (firstChild = node.firstChild) {
+                removeChild(firstChild, node);
+              }
+            }
+          } else {
+            // Traverse and Diff Children
+            var totalLen = newLength > oldLength ? newLength : oldLength;
+            for (var i = 0; i < totalLen; i++) {
+              diff(oldVNode.children[i], vnode.children[i], node, instance);
+            }
           }
         }
     
