e954bc84ee7e67c0848d59ac387951ede49e04b9
Kabir Shah <simplecooldude1@gmail.com>
2017-05-19 22:03:41 -0700

    add search in docs

--- js/post.js
+++ js/post.js
@@ -1,3 +1,10 @@
+docsearch({
+  apiKey: '7710c47b3b47d4ee65253d66582eccb1',
+  indexName: 'moonjs',
+  inputSelector: '#search',
+  debug: false
+});
+
 var sidebarToggle = document.getElementById("sidebar-toggle");
 
 sidebarToggle.addEventListener("click", function() {
--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ docsearch({
   apiKey: '7710c47b3b47d4ee65253d66582eccb1',
   indexName: 'moonjs',
   inputSelector: '#search',
-  debug: true
+  debug: false
 });
 
 var app = new Moon({
