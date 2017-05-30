f44c18e7a18e3feb628587866056277744301504
Adekoyejo Akinhanmi <koyesavedbygrace@yahoo.com>
2017-05-30 22:49:17 +0100

    completed github commit example

--- examples/github-commits/scripts.js
+++ examples/github-commits/scripts.js
@@ -1,9 +1,28 @@
-const apiUrl = 'https://api.github.com/repos/kingpixil/moon/commits?per_page=3&sha=';
+/*jshint esversion : 6*/
+/*global window, XMLHttpRequest, Moon, console*/
+var apiUrl = 'https://api.github.com/repos/kingpixil/moon/commits?per_page=3&sha=';
 
-const app = new Moon({
+var app = new Moon({
     el : "#app",
     data : {
         branches : ['master', 'gh-pages'],
-        currentBranch : ''
+        currentBranch : 'master',
+        commits : []
+    },
+    methods : {
+        fetchCommits : function () {
+            var branch = this.get('currentBranch');
+            var self = this;
+            
+            axios.get(apiUrl + branch).then(function(response) {
+                self.set('commits', response.data);
+            }).catch(function (error) {
+                console.log(error);
+            });
+        }
     }
 });
+
+window.onload = function () {
+    app.callMethod('fetchCommits', []);
+};
