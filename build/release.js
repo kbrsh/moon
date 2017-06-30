6d04abd8bcf45c9533ad2c0ddfc21b5f8a60800d
Kabir Shah <simplecooldude1@gmail.com>
2017-06-30 14:56:32 +0530

    update release script

--- build/release.js
+++ build/release.js
@@ -6,8 +6,8 @@ tags = tags.slice(-2);
 
 let commits = exec(`git log --pretty=oneline ${tags[0]}...${tags[1]}`).toString().split("\n");
 commits.pop();
-
-commits = ["330fc4be4b5cb55c29270a0f0673316f93cf0b4a perf: diff a component faster", "53412707abd5f22a872b107b3df8d575f515e5a0 feat: allow options in plugins", "2de02db7b327020f8fd05e53a0120634c3480e7b fix: handle null children when creating nodes from vnodes"]
+console.log(commits)
+// commits = ["330fc4be4b5cb55c29270a0f0673316f93cf0b4a perf: diff a component faster", "53412707abd5f22a872b107b3df8d575f515e5a0 feat: allow options in plugins", "2de02db7b327020f8fd05e53a0120634c3480e7b fix: handle null children when creating nodes from vnodes", "14d787697bde83be1a19a4a92ce7ba91cd086699 feat: improve parser by allowing custom elements that self close, but throw if there is no slash to indicate it", "f304213f7dc7ea597c72e4ac256002d8caadba27 diff: make dom props faster"]
 
 let code = "", i = 0, commit, commitEntry, hash, category, patches = [], features = [], performance = [], breaking = [];
 
@@ -15,9 +15,11 @@ for(; i < commits.length; i++) {
 	commit = commits[i];
 	commit = commit.split(" ");
 	hash = commit.shift();
-	commit = commit.join(" ");
+	commit = commit.join(" ").split(":");
 
-	category = commit.split(":").shift();
+	category = commit.shift();
+
+	commit = commit.join(":");
 
 	commitEntry = {
 		hash: hash,
@@ -47,7 +49,7 @@ const generateCategory = (header, entries) => {
 		code += `${first === false ? "\n\n" : ""}# ${header}\n\n`;
 		for(i = 0; i < entries.length; i++) {
 			entry = entries[i];
-			code += `* ${entry.hash} ${entry.message}`;
+			code += `*${entry.message} - ${entry.hash}`;
 		}
 		first = false;
 	}
