7b329e9de9a3a06c28a00d93fe756111d09aa443
Kabir Shah <simplecooldude1@gmail.com>
2017-06-30 15:49:01 +0530

    make release script generate output for multiple commits in single category

--- build/release.js
+++ build/release.js
@@ -1,4 +1,6 @@
 const exec = require("child_process").execSync;
+const fs = require("fs");
+const path = require("path");
 
 let tags = exec("git tag --sort=committerdate").toString().split("\n");
 tags.pop();
@@ -6,8 +8,8 @@ tags = tags.slice(-2);
 
 let commits = exec(`git log --pretty=oneline ${tags[0]}...${tags[1]}`).toString().split("\n");
 commits.pop();
-console.log(commits)
-// commits = ["330fc4be4b5cb55c29270a0f0673316f93cf0b4a perf: diff a component faster", "53412707abd5f22a872b107b3df8d575f515e5a0 feat: allow options in plugins", "2de02db7b327020f8fd05e53a0120634c3480e7b fix: handle null children when creating nodes from vnodes", "14d787697bde83be1a19a4a92ce7ba91cd086699 feat: improve parser by allowing custom elements that self close, but throw if there is no slash to indicate it", "f304213f7dc7ea597c72e4ac256002d8caadba27 diff: make dom props faster"]
+
+commits = ["330fc4be4b5cb55c29270a0f0673316f93cf0b4a perf: diff a component faster", "53412707abd5f22a872b107b3df8d575f515e5a0 feat: allow options in plugins", "2de02db7b327020f8fd05e53a0120634c3480e7b fix: handle null children when creating nodes from vnodes", "14d787697bde83be1a19a4a92ce7ba91cd086699 feat: improve parser by allowing custom elements that self close, but throw if there is no slash to indicate it", "f304213f7dc7ea597c72e4ac256002d8caadba27 breaking: make dom props faster"]
 
 let code = "", i = 0, commit, commitEntry, hash, category, patches = [], features = [], performance = [], breaking = [];
 
@@ -46,12 +48,20 @@ let entry, first = true;
 
 const generateCategory = (header, entries) => {
 	if(entries.length > 0) {
-		code += `${first === false ? "\n\n" : ""}# ${header}\n\n`;
-		for(i = 0; i < entries.length; i++) {
-			entry = entries[i];
-			code += `*${entry.message} - ${entry.hash}`;
+		if(first === false) {
+			code += `\n\n### ${header}\n\n`;
+		} else {
+			code += `### ${header}\n\n`;
+			first = false;
 		}
-		first = false;
+
+		for(i = 0; i < entries.length - 1; i++) {
+			entry = entries[i];
+			code += `*${entry.message} - ${entry.hash}\n`;
+		}
+
+		entry = entries[i];
+		code += `*${entry.message} - ${entry.hash}`;
 	}
 }
 
@@ -60,4 +70,8 @@ generateCategory("Features", features);
 generateCategory("Patches", patches);
 generateCategory("Performance", performance);
 
-console.log(code);
+const releaseNotePath = path.resolve("./RELEASE_NOTE");
+fs.writeFileSync(releaseNotePath, code);
+exec(`cat ./RELEASE_NOTE | pbcopy`);
+fs.unlinkSync(releaseNotePath);
+exec(`open https://github.com/KingPixil/moon/releases/edit/${tags[1]}`);
