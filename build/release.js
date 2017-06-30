97a591d0fd3dbb3142419afc2789f018393c8bb0
Kabir Shah <simplecooldude1@gmail.com>
2017-06-30 16:06:09 +0530

    remove testing commits from release script

--- build/release.js
+++ build/release.js
@@ -9,8 +9,6 @@ tags = tags.slice(-2);
 let commits = exec(`git log --pretty=oneline ${tags[0]}...${tags[1]}`).toString().split("\n");
 commits.pop();
 
-commits = ["330fc4be4b5cb55c29270a0f0673316f93cf0b4a perf: diff a component faster\ncache objects to avoid lookups inside of a loop helps with diffing components. also use a for loop instead of 'map'", "53412707abd5f22a872b107b3df8d575f515e5a0 feat: allow options in plugins", "2de02db7b327020f8fd05e53a0120634c3480e7b fix: handle null children when creating nodes from vnodes", "14d787697bde83be1a19a4a92ce7ba91cd086699 feat: improve parser by allowing custom elements that self close, but throw if there is no slash to indicate it", "f304213f7dc7ea597c72e4ac256002d8caadba27 breaking: make dom props faster"]
-
 let code = "", i = 0, commit, commitEntry, hash, category, patches = [], features = [], performance = [], breaking = [];
 
 for(; i < commits.length; i++) {
