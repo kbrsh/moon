19e557120a614510a95319ecbe1d348325c9529c
Kabir Shah <simplecooldude1@gmail.com>
2017-07-07 15:59:08 -0700

    add tests for m-for with a range

--- test/core/directives/for.js
+++ test/core/directives/for.js
@@ -66,4 +66,35 @@ describe('For Directive', function() {
       expect(ul.childNodes[3].getAttribute("m-for")).to.be['null'];
     });
   });
+
+  describe("Range", function() {
+    var forRangeEl = createTestElement("forRange", "<ul><li m-for='item in range'>{{item}}</li></ul>");
+    var ul = forObjectEl.firstChild;
+
+    var app = new Moon({
+      el: "#forRange",
+      data: {
+        range: 3
+      }
+    });
+
+    it('should render a list', function() {
+      expect(ul.childNodes.length).to.equal(3);
+    });
+
+    it('should update a list', function() {
+      app.set("range", 4);
+
+      return wait(function() {
+        expect(ul.childNodes.length).to.equal(4);
+      });
+    });
+
+    it('should not be present at runtime', function() {
+      expect(ul.childNodes[0].getAttribute("m-for")).to.be['null'];
+      expect(ul.childNodes[1].getAttribute("m-for")).to.be['null'];
+      expect(ul.childNodes[2].getAttribute("m-for")).to.be['null'];
+      expect(ul.childNodes[3].getAttribute("m-for")).to.be['null'];
+    });
+  });
 });
