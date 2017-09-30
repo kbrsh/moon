3ab98797c30e50746bec44bab6ee54026f634739
Kabir Shah <iamkbrsh@gmail.com>
2017-09-29 18:35:16 -0700

    add tests for computed property cache

--- test/core/instance/computed.js
+++ test/core/instance/computed.js
@@ -1,30 +1,43 @@
-describe('Computed', function() {
-  var computed = createTestElement("computed", '<p>{{msg}}</p><p>{{reversed}}</p>');
+describe("Computed", function() {
+  var computed = createTestElement("computed", "<p>{{msg}}</p><p>{{reversed}}</p>");
+  var called = false;
 
   var app = new Moon({
     root: "#computed",
     data: {
-      msg: "Message"
+      msg: "Message",
+      foo: false
     },
     computed: {
       reversed: {
         get: function() {
-          return this.get('msg').split("").reverse().join("");
+          called = true;
+          return this.get("msg").split("").reverse().join("");
         }
       }
     }
   });
 
-  it('should compute at initial render', function() {
+  it("should compute at initial render", function() {
     return wait(function() {
       expect(computed.childNodes[1].textContent).to.equal("egasseM");
     });
   });
 
-  it('should update when the message updates', function() {
-    app.set('msg', 'New');
+  it("should update when the message updates", function() {
+    app.set("msg", "New");
     return wait(function() {
-      expect(computed.childNodes[1].textContent).to.equal('weN');
+      expect(computed.childNodes[1].textContent).to.equal("weN");
+    });
+  });
+
+  it("should cache the value if a non-dependency is changed", function() {
+    called = false;
+    app.set("foo", true);
+
+    return wait(function() {
+      expect(called).to.be["false"];
+      expect(computed.childNodes[1].textContent).to.equal("weN");
     });
   });
 });
