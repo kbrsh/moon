e3b28565f470b8239730b2fccc7ffca098c39f8c
Kabir Shah <iamkbrsh@gmail.com>
2018-04-22 18:31:23 -0700

    add plugins and env

--- build/build.js
+++ build/build.js
@@ -1,9 +1,11 @@
 const rollup = require("rollup");
+const buble = require("rollup-plugin-buble");
 const uglify = require("uglify-js");
 const fs = require("fs");
 const path = require("path");
 const pkg = require("../package.json");
 const cwd = process.cwd();
+const ENV_RE = /__ENV__/g;
 
 const comment = `/**
  * Moon v${pkg.version}
@@ -19,14 +21,17 @@ const options = {
 
 async function build() {
   const bundle = await rollup.rollup({
-    input: path.join(cwd, "/src/index.js")
+    input: path.join(cwd, "/src/index.js"),
+    plugins: [
+      buble()
+    ]
   });
 
   let { code } = await bundle.generate(options);
   code = fs.readFileSync(path.join(cwd, "/src/wrapper.js")).toString().replace("INSERT", code.split("\n").slice(1, -3).join("\n")).replace("'use strict'", "\"use strict\"");
 
-  fs.writeFileSync(path.join(cwd, "/dist/moon.js"), comment + code);
-  fs.writeFileSync(path.join(cwd, "/dist/moon.min.js"), comment + uglify.minify(code).code);
+  fs.writeFileSync(path.join(cwd, "/dist/moon.js"), comment + code.replace(ENV_RE, '"development"'));
+  fs.writeFileSync(path.join(cwd, "/dist/moon.min.js"), comment + uglify.minify(code.replace(ENV_RE, '"production"')).code);
 }
 
 build();
