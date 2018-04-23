5285c631717ba99e52f454fb3a820ff15c84558f
Kabir Shah <iamkbrsh@gmail.com>
2018-04-22 18:36:20 -0700

    use env variable MOON_ENV

--- build/build.js
+++ build/build.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 const pkg = require("../package.json");
 const cwd = process.cwd();
-const ENV_RE = /__ENV__/g;
+const ENV_RE = /process\.env\.MOON_ENV/g;
 
 const comment = `/**
  * Moon v${pkg.version}
