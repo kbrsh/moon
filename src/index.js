a8a52fcceaf2d0c7045efca5aebb782591441318
Kabir Shah <iamkbrsh@gmail.com>
2018-04-25 22:50:57 -0700

    add component and root instance constructor

--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
 import { compile } from "./compiler/compiler";
 import { config } from "./util/config";
 
-export default function Moon() {
+let components = {};
 
+export default function Moon(element, view) {
+  if (typeof element === "string") {
+    element = document.querySelector(element);
+  }
+
+  if (typeof view === "string") {
+    view = compile(view);
+  }
+
+  view[0]();
+  view[1]();
 }
+
+Moon.extend = (name, view, data) => {
+  if (typeof view === "string") {
+    view = compile(view);
+  }
+
+  components[name] = () => {
+    return {
+      name: name,
+      data: data,
+      mount: view[0],
+      build: view[1],
+      m: []
+    };
+  };
+};
 
 Moon.compile = compile;
 Moon.config = config;
