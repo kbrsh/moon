107c1e54ec43d18398cb287faa4203acaff7ae73
Kabir Shah <iamkbrsh@gmail.com>
2018-04-25 18:57:00 -0700

    remove global

--- src/global/global.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-  silent: (process.env.MOON_ENV === "production") || (typeof console === "undefined")
-};
-
-export const createGlobal = (Moon) => {
-  Moon.config = config;
-};
