69fdd4266d5ccc591ed1bc04516431a638c4454e
Kabir Shah <iamkbrsh@gmail.com>
2018-04-25 22:52:31 -0700

    add basic incomplete generator

--- src/compiler/generator.js
+++ src/compiler/generator.js
@@ -1,1 +1,29 @@
-export const generate = (tree) => {};
+const generateCreateFragment = (element) => {
+  return `${element.children.map(generateCreate)} m[${element.index}] = []; `;
+};
+
+const generateCreateText = (element) => {};
+
+const generateCreateElement = (element) => {
+  return ` m[${element.index}] = document.createElement("${element.type}");`;
+};
+
+const generateCreate = (element) => {
+  switch (element.type) {
+    case "m-fragment":
+      return generateCreateFragment(element);
+      break;
+    case "m-text":
+      return generateCreateText(element);
+      break;
+    default:
+      return generateCreateElement(element);
+  }
+};
+
+const generateUpdate = () => {};
+
+export const generate = (tree) => {
+  const prelude = `var data = instance.data; var m = instance.m;`;
+  return new Function(`return [function (instance) {${prelude}${generateCreate(tree)}}, function (instance) {${prelude}${generateUpdate(tree)}}]`)();
+};
