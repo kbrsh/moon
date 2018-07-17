f1630ece082817a3a95d8fd4cc1232ba3e92e03d
Kabir Shah <iamkbrsh@gmail.com>
2018-07-17 12:42:24 -0700

    add mvl

---
+++ packages/moon-mvl/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const Moon = require("moon");
+
+module.exports = function(file, contents) {
+	let js = "import Moon from \"moon\";";
+	let css = "";
+	let dep;
+
+	const view = "function(m,instance,locals){" + Moon.compile(this.contents) + "};";
+	let data = "{};";
+
+	const fileName = path.basename(file).slice(0, -4);
+	const directoryName = path.dirname(file);
+	if (fs.existsSync(path.join(directoryName, fileName + ".js"))) {
+		dep = `.${path.sep}${fileName}.js`;
+		js += `import data from "${dep}";`;
+		data = "data;";
+	}
+
+	js += `export default Moon.extend("${path.basename(directoryName)}",function(){var options=${data}options.view=${view}return options;});`;
+
+	return {
+		js: js,
+		css: css,
+		dep: dep
+	};
+};
