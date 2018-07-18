6e73fbb269d2803d878400d4e320e3d3a6d68e02
Kabir Shah <iamkbrsh@gmail.com>
2018-07-18 11:00:59 -0700

    export libraries

---
+++ packages/moon-mvl/lib/css.js
@@ -0,0 +1,10 @@
+const css = (code) => {
+	const head = document.querySelector("head");
+	const style = document.createElement("style");
+	style.appendChild(document.createTextNode(code));
+	head.appendChild(style);
+
+	return () => {
+		head.removeChild(style);
+	};
+};
