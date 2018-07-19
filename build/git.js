3d3bbe6886022d255cb53b3a9ee7ebd51c56c427
Kabir Shah <iamkbrsh@gmail.com>
2018-07-18 17:07:41 -0700

    add build scripts

--- build/git.js
+++ build/git.js
@@ -4,19 +4,6 @@ const exec = require("child_process").execSync;
 
 const version = process.argv[2];
 
-const packages = fs.readdirSync("./packages");
-for (let i = 0; i < packages.length; i++) {
-	const package = path.join("./packages", packages[i]);
-	if (fs.statSync(package).isDirectory()) {
-		const packageJSONPath = path.join(package, "package.json");
-		const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath).toString());
-		packageJSON.version = version;
-		fs.writeFileSync(packageJSONPath, JSON.stringify(packageJSON, null, 2));
-	}
-}
-
-console.log(exec("npm run build").toString());
-
 const trailingNewlinesRE = /\n+$/;
 const trailingLinesRE = /\n([^\n]*)/g;
 
---
+++ build/start.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const exec = require("child_process").execSync;
+
+const version = process.argv[2];
+
+const packages = fs.readdirSync("./packages");
+for (let i = 0; i < packages.length; i++) {
+	const package = path.join("./packages", packages[i]);
+	if (fs.statSync(package).isDirectory()) {
+		const packageJSONPath = path.join(package, "package.json");
+		const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath).toString());
+		packageJSON.version = version;
+		fs.writeFileSync(packageJSONPath, JSON.stringify(packageJSON, null, 2));
+	}
+}
+
+console.log(exec("npm run build").toString());
