fe8149ec93a18b59df89d234a60532aee5948a15
Kabir Shah <iamkbrsh@gmail.com>
2019-04-04 01:13:20 -0700

    add lexer tests

--- packages/moon/test/compiler/lexer.test.js
+++ packages/moon/test/compiler/lexer.test.js
@@ -1,4 +1,4 @@
-import { lex } from "../../src/compiler/lexer/lexer";
+import { lex, tokenString } from "../../src/compiler/lexer/lexer";
 
 test("lex opening tag", () => {
 	expect(lex(`<div>`)).toEqual([{"attributes": {}, "closed": false, "type": "tagOpen", "value": "div"}]);
@@ -49,4 +49,39 @@ test("lex attributes", () => {
 
 test("lex comments", () => {
 	expect(lex(`<!-- comment -->`)).toEqual([]);
+});
+
+test("opening tag token to string", () => {
+	const input = "<div>";
+	expect(tokenString(lex(input)[0])).toBe(input);
+});
+
+test("opening tag token with attributes to string", () => {
+	const input = `<div id="test-id" class='test-class' expression={dynamic}>`;
+	expect(tokenString(lex(input)[0])).toBe(input);
+});
+
+test("self-closing tag token to string", () => {
+	const input = `<input/>`;
+	expect(tokenString(lex(input)[0])).toBe(input);
+});
+
+test("self-closing tag token with attributes to string", () => {
+	const input = `<input id="test-id" class='test-class' expression={dynamic}/>`;
+	expect(tokenString(lex(input)[0])).toBe(input);
+});
+
+test("closing tag token to string", () => {
+	const input = `</div>`;
+	expect(tokenString(lex(input)[0])).toBe(input);
+});
+
+test("text token to string", () => {
+	const input = `Test Text`;
+	expect(tokenString(lex(input)[0])).toBe(input);
+});
+
+test("expression token to string", () => {
+	const input = `{dynamic + 1}`;
+	expect(tokenString(lex(input)[0])).toBe(input);
 });
