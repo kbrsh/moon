517792b3457ec296ec341ebd7e648ccec674b94a
Kabir Shah <iamkbrsh@gmail.com>
2019-06-18 21:01:06 -0700

    add code editor

--- play/js/index.js
+++ play/js/index.js
@@ -1,4 +1,23 @@
 var MoonMVL = module.exports;
 
-var MVLEditor = CodeMirror(document.getElementById("editor-mvl"));
-var JSEditor = CodeMirror(document.getElementById("editor-javascript"));
+function merge(obj1, obj2) {
+	for (var key in obj2) {
+		obj1[key] = obj2[key];
+	}
+
+	return obj1;
+}
+
+var config = {
+	theme: "nox",
+	lineNumbers: true,
+	indentWithTabs: true
+};
+
+var MVLEditor = CodeMirror(document.getElementById("editor-mvl"), merge(config, {
+	mode: "htmlmixed"
+}));
+
+var JSEditor = CodeMirror(document.getElementById("editor-javascript"), merge(config, {
+	mode: "javascript"
+}));
