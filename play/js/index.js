85c5955d337863acb6d54ba2634cd456e76f0d04
Kabir Shah <iamkbrsh@gmail.com>
2019-07-26 14:11:39 -0700

    init new playground

--- play/js/index.js
+++ play/js/index.js
@@ -1,38 +1,7 @@
-var MoonMVL = module.exports;
-
 var config = {
 	theme: "nox",
-	mode: "htmlmixed",
-	value: `<div>
-	<h1>Todo List</h1>
-	<for={$todo, $index} of={todos} name="ul">
-		<li @click={() => { remove($index); }}>{$todo}</li>
-	</for>
-	<input placeholder="What do you need to do?" @keydown={append}/>
-</div>
-
-<script>
-	export default {
-		data: {
-			todos: ["Learn Moon", "Build an application", "Take a nap"],
-
-			append(event, { todos }) {
-				if (event.keyCode === 13) {
-					Moon.set({
-						todos: todos.concat([event.target.value])
-					});
-
-					event.target.value = "";
-				}
-			},
-			remove(index) {
-				Moon.set({
-					todos: todos.filter((todo, todoIndex) => index !== todoIndex)
-				});
-			}
-		}
-	};
-</script>`,
+	mode: "javascript",
+	value: `alert("hello world");`,
 	lineNumbers: true,
 	indentWithTabs: true
 };
@@ -41,20 +10,16 @@ var editor = CodeMirror(document.getElementById("editor"), config);
 var result = document.getElementById("result");
 
 function render() {
-	var value = editor.getValue();
-	var compiled = MoonMVL("Root", value);
 	result.srcdoc = `
 		<!DOCTYPE html>
 		<html>
 			<head>
 				<title>Moon | Playground Result</title>
-
-				<style>${compiled.css}</style>
 			</head>
 			<body>
 				<div id="root"></div>
 				<script src="/play/js/lib/moon.js"></script>
-				<script>${compiled.js.replace(`import Moon from "moon";`, "").replace("Moon(_moonOptions);", `_moonOptions.root="#root";Moon(_moonOptions);`)}</script>
+				<script>${MoonCompiler.compile(editor.getValue())}</script>
 			</body>
 		</html>
 	`;
